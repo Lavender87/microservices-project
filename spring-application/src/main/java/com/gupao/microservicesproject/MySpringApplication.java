b5932cedac48a361f188f96238f6f4d1af0be90d
Lavender87 <shuiliansuixin@126.com>
2020-03-28 22:16:02 +0800

    add spring-boot config

--- spring-application/src/main/java/com/gupao/microservicesproject/MySpringApplication.java
+++ spring-application/src/main/java/com/gupao/microservicesproject/MySpringApplication.java
@@ -63,9 +63,51 @@ public class MySpringApplication {
 //
 //		{"query":"query \n{\n    countBooks\n}","variables":{}}
 
-		OkHttpUtils.testGraphql();
+//		OkHttpUtils.testGraphql();
 
 //		testDataTransfer();
+
+//		dataTransfer();
+	}
+
+
+	public static void dataTransfer(){
+		BufferedWriter bw = null;
+		BufferedReader reader = null;
+		try {
+//			写文件
+			File csv = new File("D:\\test\\writers.csv"); // CSV数据文件
+			bw = new BufferedWriter(new FileWriter(csv, true));
+			//读文件
+			reader = new BufferedReader(new FileReader("D:\\test\\read.csv"));//换成你的文件名
+			reader.readLine();//第一行信息，为标题信息，不用,如果需要，注释掉
+			String line = null;
+			while((line=reader.readLine())!=null){
+				String item[] = line.split(",");//CSV格式文件为逗号分隔符文件，这里根据逗号切分
+
+				String num = item[item.length-1];//这就是你要的数据了
+				String myText = num+ "," +"张三" + "," + "2000" + "," + "2004";
+//				int value = Integer.parseInt(last);//如果是数值，可以转化为数值
+//				System.out.println(last);
+
+				// 添加新的数据行
+				bw.write(myText);
+				bw.newLine();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			try {
+				bw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	public static void test1(){
